4d16a63f27b63922c609ecdcf9407416286bca31
Tom Hart <tommy-hart@hotmail.com>
2022-04-22 11:13:21 +0100

    basic homepage functionality

---
+++ resources/views/homepage.blade.php
@@ -0,0 +1,11 @@
+<x-layout>
+
+    <body style="font-family: Open Sans, sans-serif">
+        <section class="px-6 py-8">
+            <main class="max-w-6xl mx-auto mt-10 lg:mt-20 space-y-6">
+                <div>Hello World</div>
+            </main>
+
+        </section>
+    </body>
+</x-layout>
