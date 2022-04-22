9781ec2db3dd7e4fa7bc0d89634940abad9ce23b
Tom Hart <tommy-hart@hotmail.com>
2022-04-22 12:35:43 +0100

    tweak homepage to display logged in user's name

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -3,7 +3,13 @@
     <body style="font-family: Open Sans, sans-serif">
         <section class="px-6 py-8">
             <main class="max-w-6xl mx-auto mt-10 lg:mt-20 space-y-6">
-                <div>Hello World</div>
+                <div>
+                    @auth
+                    <p>Welcome, {{ auth()->user()->name }}!</p>
+                    @else
+                    <p>Please log in to view content.</p>
+                    @endauth
+                </div>
             </main>
 
         </section>
