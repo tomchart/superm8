04b1247d156cdda4d301eb6920cc422a3ab2d12a
Tom Hart <tommy-hart@hotmail.com>
2022-04-22 14:44:02 +0100

    label upper --> lower

--- resources/views/components/form/label.blade.php
+++ resources/views/components/form/label.blade.php
@@ -1,4 +1,4 @@
 @props (['name'])
-<label class="block mb-2 uppercase font-bold text-xs text-gray-700" for="{{ $name }}">
+<label class="block mb-2 lowercase font-bold text-xs text-gray-700" for="{{ $name }}">
     {{ ucwords($name) }}
 </label>
