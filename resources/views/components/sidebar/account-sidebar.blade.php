060551e41228856b17ccd3c991a625ee72925bdf
Tom Hart <tommy-hart@hotmail.com>
2022-04-25 16:47:28 +0100

    component design tweaks

--- resources/views/components/sidebar/account-sidebar.blade.php
+++ resources/views/components/sidebar/account-sidebar.blade.php
@@ -1,4 +1,4 @@
-<div class="lg:grid lg:grid-cols-12">
+<div class="lg:grid lg:grid-cols-12 px-6 py-8">
     <x-panel class="col-span-3 lg:pt-14 mb-10 mr-4">
         <p>Account Management: {{ auth()->user()->username }}</p>
         <x-sidebar.link url="home" str="Home"></x-sidebar.link>
