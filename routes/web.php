57862933c9837163c4899752bbcac8cd53685e92
Tom Hart <tommy-hart@hotmail.com>
2022-04-25 13:47:23 +0100

    add AccountController and route, add /home redirect route

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,13 @@
 use App\Http\Controllers\RegisterController;
 use App\Http\Controllers\SessionController;
 use App\Http\Controllers\ClubController;
+use App\Http\Controllers\AccountController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
+    return view('homepage');
+});
+Route::get('/home', function () {
     return view('homepage');
 });
 
@@ -26,3 +30,8 @@ Route::get('/logout', [SessionController::class, 'destroy'])->middleware('auth')
 //// Clubs
 // View detail page
 Route::get('/club/{club:slug}', [ClubController::class, 'show'])->middleware(['auth', 'member']);
+
+
+//// Account
+// Show
+Route::get('/account', [AccountController::class, 'show'])->middleware('auth');
