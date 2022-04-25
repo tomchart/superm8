6e384b05471cb15d9b8218793ee5399e4c3d0247
Tom Hart <tommy-hart@hotmail.com>
2022-04-25 16:48:54 +0100

    use sidebar in homepage view

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,10 +1,6 @@
 <x-layout>
-    <section class="px-6 py-8">
-        <main class="">
-            <x-sidebar.home-sidebar>
-                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et enim tincidunt, scelerisque quam eu, posuere diam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc tristique finibus efficitur. Vivamus egestas lorem purus, at vestibulum tortor interdum eget. Donec nec sodales sapien, eu semper risus. Mauris rutrum dapibus justo, vestibulum elementum metus ullamcorper id. Mauris placerat semper velit ac mattis. In tortor leo, consectetur ut velit ut, vulputate egestas ipsum. Fusce gravida lectus quis lectus posuere egestas eget ac elit. Ut sit amet dolor rutrum, condimentum justo non, interdum nunc.</p>
-                <p class="mt-8">Integer interdum ante nulla, sollicitudin consequat nulla lacinia at. Vivamus vestibulum mauris odio. Aenean hendrerit ultricies nibh. Praesent pretium consectetur purus, eu volutpat erat tincidunt ac. Praesent tellus ipsum, hendrerit at vulputate vitae, ultricies quis massa. Sed varius dignissim ornare. Integer ultricies, justo a bibendum maximus, elit augue sagittis nunc, a elementum ipsum ex ut elit. Aliquam dapibus, leo eget tincidunt dignissim, massa massa lobortis tortor, in pharetra turpis dui non arcu. Nam diam tortor, volutpat ut ullamcorper ac, suscipit vel augue. Ut orci purus, dictum vel leo eget, feugiat maximus dolor.</p>
-            </x-sidebar.home-sidebar>
-        </main>
-    </section>
+    <x-sidebar.home-sidebar>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur et enim tincidunt, scelerisque quam eu, posuere diam. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nunc tristique finibus efficitur. Vivamus egestas lorem purus, at vestibulum tortor interdum eget. Donec nec sodales sapien, eu semper risus. Mauris rutrum dapibus justo, vestibulum elementum metus ullamcorper id. Mauris placerat semper velit ac mattis. In tortor leo, consectetur ut velit ut, vulputate egestas ipsum. Fusce gravida lectus quis lectus posuere egestas eget ac elit. Ut sit amet dolor rutrum, condimentum justo non, interdum nunc.</p>
+        <p class="mt-8">Integer interdum ante nulla, sollicitudin consequat nulla lacinia at. Vivamus vestibulum mauris odio. Aenean hendrerit ultricies nibh. Praesent pretium consectetur purus, eu volutpat erat tincidunt ac. Praesent tellus ipsum, hendrerit at vulputate vitae, ultricies quis massa. Sed varius dignissim ornare. Integer ultricies, justo a bibendum maximus, elit augue sagittis nunc, a elementum ipsum ex ut elit. Aliquam dapibus, leo eget tincidunt dignissim, massa massa lobortis tortor, in pharetra turpis dui non arcu. Nam diam tortor, volutpat ut ullamcorper ac, suscipit vel augue. Ut orci purus, dictum vel leo eget, feugiat maximus dolor.</p>
+    </x-sidebar.home-sidebar>
 </x-layout>
