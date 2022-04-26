7ba2992714ad4e20f87d4df6835c8bf3d8e9f088
Tom Hart <tommy-hart@hotmail.com>
2022-04-26 13:30:15 +0100

    remove admin functionality from ClubController

--- app/Http/Controllers/ClubController.php
+++ app/Http/Controllers/ClubController.php
@@ -3,8 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Models\Club;
-use Illuminate\Validation\ValidationException;
-use Illuminate\Validation\Rule;
 
 class ClubController extends Controller
 {
@@ -15,36 +13,11 @@ class ClubController extends Controller
         ]);
     }
 
-    public function create()
+    public function index()
     {
-        return view('club.create');
-    }
-
-    public function store()
-    {
-        $attributes = array_merge($this->validateClub(), [
-            'owner' => auth()->id(),
-        ]);
-
-        if (!($attributes)) {
-            throw ValidationException::withMessages([
-                'name' => "Your club name could not be verified",
-                'slug' => "Your club's slug could not be verified",
-            ]);
-        }
-
-        $club = Club::create($attributes);
-        auth()->user()->clubs()->attach($club);
-
-        return redirect('/club/' . $club->slug);
-    }
-
-    protected function validateClub(?Club $club = null): array
-    {
-        $club ??= new Club();
-        return request()->validate([
-            'name' => ['required', 'max:255'],
-            'slug' => ['required', 'max:255', Rule::unique('clubs', 'slug')->ignore($club->id)],
+        $clubs = auth()->user()->clubs;
+        return view('club.index', [
+            'clubs' => $clubs,
         ]);
     }
 }
