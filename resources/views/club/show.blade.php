ff1d9e0da3cfda87c02c937eedd087e9c78c26d2
Tom Hart <tommy-hart@hotmail.com>
2022-04-26 13:32:48 +0100

    add edit button to club.show blade

--- resources/views/club/show.blade.php
+++ resources/views/club/show.blade.php
@@ -6,15 +6,21 @@
         <li>{{ $user->name }}</li>
         @endforeach
 
-        <form method="POST" action="/invite/{{ $club->slug }}" class="mt-10">
-            @csrf
-            <div class="inline-flex">
-                <x-form.button>create invite</x-form.button>
-                @if (session()->has('invite'))
-                <p class="py-2 px-8 mt-6">Invite code: {{ session('invite')->code }}</p>
-                @endif
-            </div>
-        </form>
+        <div class="inline-flex">
+            <!-- make this only for club owner with a Gate -->
+            <a href="/admin/club/{{ $club->id }}/edit" class="mt-16 py-2 px-4 mr-8 hover:underline">edit club</a>
+
+            <form method="POST" action="/invite/{{ $club->slug }}" class="mt-10">
+                @csrf
+                <div>
+                    <x-form.button>create invite</x-form.button>
+                    @if (session()->has('invite'))
+                    <p class="py-2 px-8 mt-6">Invite code: {{ session('invite')->code }}</p>
+                    @endif
+                </div>
+            </form>
+
+        </div>
 
     </x-sidebar.home-sidebar>
 </x-layout>
