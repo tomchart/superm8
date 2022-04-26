fd1b12ea4e8a571d55c7f1c50e2eda1d3932a52a
Tom Hart <tommy-hart@hotmail.com>
2022-04-26 21:01:17 +0100

    add margin to form.button directly (possible revert)

--- resources/views/components/form/button.blade.php
+++ resources/views/components/form/button.blade.php
@@ -1,5 +1,5 @@
 <x-form.field>
-    <button type="submit" class="bg-gray-600 text-white rounded-full py-2 px-8 hover:bg-gray-500">
+    <button type="submit" class="bg-gray-600 text-white rounded-full py-2 px-8 hover:bg-gray-500 mt-6">
         {{ $slot }}
     </button>
 </x-form.field>
