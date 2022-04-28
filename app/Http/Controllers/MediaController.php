02a87dbdd7a996235f1bc9484651747117d4fea0
Tom Hart <tommy-hart@hotmail.com>
2022-04-28 17:07:03 +0100

    start writing MediaController

--- app/Http/Controllers/MediaController.php
+++ app/Http/Controllers/MediaController.php
@@ -7,11 +7,19 @@ use Illuminate\Validation\Rule;
 
 class MediaController extends Controller
 {
+    public function store()
+    {
+        // validate new media
+        $media = $this->validateMedia();
+        // store new instance
+    }
+
     protected function validateMedia(?Media $media = null): array
     {
         $media ??= new Media();
         return request()->validate([
             'name' => ['required', 'max:255', Rule::unique('media', 'name')->ignore($media->id)],
+            'type_id' => ['required', Rule::exists('media_type', 'id')],
         ]);
     }
 }
