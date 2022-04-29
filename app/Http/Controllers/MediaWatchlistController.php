8847dce60be21bb17f332d7d21bb635caccc555f
Tom Hart <tommy-hart@hotmail.com>
2022-04-29 17:50:11 +0100

    add MediaWatchlistController

---
+++ app/Http/Controllers/MediaWatchlistController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Watchlist;
+use App\Models\Media;
+
+class MediaWatchlistController extends Controller
+{
+    public function update(Watchlist $watchlist, Media $media)
+    {
+        $watchlist->media()->updateExistingPivot($media->id, ['watched' => request()->inverse_status]);
+        return redirect(url()->previous() . '#list');
+    }
+}
