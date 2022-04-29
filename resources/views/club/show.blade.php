d7fdc4bb81700dc5fa00dad27500ec695ffb9601
Tom Hart <tommy-hart@hotmail.com>
2022-04-29 13:45:10 +0100

    add form for creating watchlist in club.show

--- resources/views/club/show.blade.php
+++ resources/views/club/show.blade.php
@@ -25,6 +25,29 @@
         <hr class="mt-6">
 
         <x-watchlist.main>
+            <x-slot name="new">
+                <form method="POST" action='/watchlist/{{ $club->id }}/create' enctype="multipart/form-data">
+                    @csrf
+                    @method('POST')
+
+                    <div class="lg:grid lg:grid-cols-12">
+                        <div class="col-span-8">
+                            <x-form.label name="watchlist name" />
+                            <x-form.input name="name" required />
+
+                        </div>
+
+                        @error('watchlist')
+                        <span class="text-xs text-red-500">{{ $message }}</span>
+                        @enderror
+
+                        <div class="inline-flex col-span-4 ml-4">
+                            <x-form.button>add</x-form.button>
+                        </div>
+
+                    </div>
+                </form>
+            </x-slot>
 
             <x-slot name="input">
                 <form method="POST" action='/watchlist/{{ $club->id }}/' enctype="multipart/form-data">
@@ -36,8 +59,6 @@
                             <x-form.label name="add something" />
                             <x-form.input name="name" required />
                         </div>
-
-
 
                         <div class="inline-flex col-span-4">
                             <select name="type_id" id="type_id" class="mt-4 ml-4 mr-4">
