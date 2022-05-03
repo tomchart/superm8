fd1d7f5d1a69b7940a669f740ffabb157c1196e7
Tom Hart <tommy-hart@hotmail.com>
2022-05-03 16:23:50 +0100

    marking watched/unwatched on watchlist attaches/detaches media to user in media_user intermediary table

--- app/Http/Controllers/MediaWatchlistController.php
+++ app/Http/Controllers/MediaWatchlistController.php
@@ -10,6 +10,20 @@ class MediaWatchlistController extends Controller
     public function update(Watchlist $watchlist, Media $media)
     {
         $watchlist->media()->updateExistingPivot($media->id, ['watched' => request()->inverse_status]);
+        // refactor this
+        if (request()->inverse_status) {
+            foreach ($watchlist->club->users as $user) {
+                if ($user->watched->where('id', $media->id)->count() == 0) {
+                    $user->watched()->attach($media);
+                };
+            };
+        } else {
+            foreach ($watchlist->club->users as $user) {
+                if ($user->watched->where('id', $media->id)->count() > 0) {
+                    $user->watched()->detach($media);
+                };
+            };
+        };
         return redirect(url()->previous() . '#list');
     }
 }
