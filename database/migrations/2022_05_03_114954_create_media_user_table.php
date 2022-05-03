feb0f1d6068111231afc1f5400c36acf270725ae
Tom Hart <tommy-hart@hotmail.com>
2022-05-03 13:13:17 +0100

    add create_media_user_table migration for intermediary many-to-many media_user table

---
+++ database/migrations/2022_05_03_114954_create_media_user_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Media;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('media_user', function (Blueprint $table) {
+            $table->foreignIdFor(Media::class);
+            $table->foreignIdFor(User::class);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('media_user');
+    }
+};
