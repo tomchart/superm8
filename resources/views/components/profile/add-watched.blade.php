b0174466297eb0a59ac85562605f6183f3fbfccd
Tom Hart <tommy-hart@hotmail.com>
2022-05-03 16:22:24 +0100

    add profile.show view and profile.add-watched component

---
+++ resources/views/components/profile/add-watched.blade.php
@@ -0,0 +1,22 @@
+<form method="POST" action='/profile/{{ auth()->user()->username }}/' enctype="multipart/form-data">
+    @csrf
+    @method('POST')
+
+    <div class="lg:grid lg:grid-cols-12">
+        <div class="col-span-8">
+            <x-form.label name="add something" />
+            <x-form.input name="name" required />
+        </div>
+
+        <div class="inline-flex col-span-4">
+            <select name="type_id" id="type_id" class="mt-4 ml-4 mr-4">
+                @foreach ($mediaTypes as $type)
+                <option value="{{ $type->id }}">{{ ucwords($type->type) }}</option>
+                @endforeach
+            </select>
+
+            <x-form.button>add</x-form.button>
+        </div>
+
+    </div>
+</form>
