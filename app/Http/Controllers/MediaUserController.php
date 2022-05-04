159801f4c880b5c9154e2455478ee3f18f20fa96
Tom Hart <tommy-hart@hotmail.com>
2022-05-04 19:14:51 +0100

    add comment to MediaUserController

--- app/Http/Controllers/MediaUserController.php
+++ app/Http/Controllers/MediaUserController.php
@@ -7,6 +7,10 @@ use App\Models\Media;
 
 class MediaUserController extends Controller
 {
+    // anyone whos anyone can post to these
+    // need a gate or a policy defined for the Models
+    // so can limit who can access
+
     public function store(User $user)
     {
         $user->media()->attach(request()->media);
