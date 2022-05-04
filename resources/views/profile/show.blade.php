1a017c59e1a29072a9ed5f618dc8f4583b5ef894
Tom Hart <tommy-hart@hotmail.com>
2022-05-04 13:58:29 +0100

    begin profile.show redesign

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,10 +1,13 @@
 <x-layout>
     @auth
     <x-sidebar.home-sidebar>
-        <p class="mb-6">{{ $user->username }}</p>
+        <p class="text-lg">{{ $user->name }}</p>
+        <p class="mb-4 italic text-sm text-gray-500">{{ '@' . $user->username }}</p>
+        <p class="text-sm">{{ $user->description }}</p>
 
-        <hr class="mt-6 mb-6" />
+        <hr class="mt-12 mb-6" />
 
+        <h2 class="font-bold underline text-lg">watched</h2>
         <x-profile.add-watched :mediaTypes="$mediaTypes" />
 
         @foreach ($user->watched as $media)
