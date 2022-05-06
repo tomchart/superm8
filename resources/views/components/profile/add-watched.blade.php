0c01a4a0503d1ca52c747ecded92d690c4623a28
Tom Hart <tommy-hart@hotmail.com>
2022-05-06 16:14:59 +0100

    add rating to profile.add-watched component

--- resources/views/components/profile/add-watched.blade.php
+++ resources/views/components/profile/add-watched.blade.php
@@ -3,15 +3,23 @@
     @method('POST')
 
     <div class="lg:grid lg:grid-cols-12">
-        <div class="col-span-8">
+        <div class="col-span-7">
             <x-form.label name="add something" />
             <x-form.input name="name" required />
         </div>
 
-        <div class="inline-flex col-span-4">
+        <div class="inline-flex col-span-5">
             <select name="type_id" id="type_id" class="mt-4 ml-4 mr-4">
                 @foreach ($mediaTypes as $type)
                 <option value="{{ $type->id }}">{{ ucwords($type->type) }}</option>
+                @endforeach
+            </select>
+
+            <select name="rating_ebert" id="rating_ebert" class="mt-4 ml-4 mr-4">
+                <option value="" disabled selected hidden>rating</option>
+                <option value="">unrated</option>
+                @foreach ($ratings as $rating)
+                <option value="{{ $rating->id }}">{{ $rating->rating }}</option>
                 @endforeach
             </select>
 
