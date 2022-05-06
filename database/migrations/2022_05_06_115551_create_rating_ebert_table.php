5e707524fdefbaff1559f81a58a15ea4de4fdb2f
Tom Hart <tommy-hart@hotmail.com>
2022-05-06 15:58:44 +0100

    add create_rating_ebert_table migration

---
+++ database/migrations/2022_05_06_115551_create_rating_ebert_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rating_ebert', function (Blueprint $table) {
+            $table->id();
+            $table->float('rating');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rating_ebert');
+    }
+};
