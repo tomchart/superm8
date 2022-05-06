844b795e02499ed2d1a25ab812bb6d45050b10b1
Tom Hart <tommy-hart@hotmail.com>
2022-05-06 16:01:45 +0100

    add foreignIdFor column to media table migration

--- database/migrations/2022_04_26_152020_create_media_table.php
+++ database/migrations/2022_04_26_152020_create_media_table.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Models\MediaType;
+use App\Models\Rating;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,6 +18,7 @@ return new class extends Migration
         Schema::create('media', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(MediaType::class, 'type_id');
+            $table->foreignIdFor(Rating::class, 'rating_ebert')->nullable();
             $table->string('name');
             $table->timestamps();
         });
