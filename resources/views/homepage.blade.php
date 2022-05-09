375fcd114c105bc1ddf284f512e51b505917f1a3
Tom Hart <tommy-hart@hotmail.com>
2022-05-09 19:57:14 +0100

    continue homepage view redesign (list clubs, films, & watched as posters)

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -1,17 +1,32 @@
 <x-layout>
     @auth
     <x-sidebar.home-sidebar>
-        <h1 class="mb-6 text-lg">unwatched</h1>
-
-        <hr class="mb-6" />
-        <h1 class="mb-6 text-lg">watched</h1>
+        <h1 class="mb-6 text-xl">Clubs & Watchlists</h1>
+        @foreach ($clubs as $club)
+        <a href="/club/{{ $club->slug }}" class="mb-6 text-xl flex hover:underline">{{ $club->name }}</a>
+        @foreach ($club->watchlists as $watchlist)
+        @if ($watchlist->media->count() > 0)
+        <a href="/club/{{ $club->slug }}#list" class="mb-6 text-xl flex hover:underline">{{ $watchlist->name }}</a>
         <div class="lg:grid lg:grid-cols-5">
-            @foreach ($user->media as $media)
-
-            <div class="col-span-1 mr-4 mb-4 transform transition duration-500 hover:scale-105">
+            @foreach ($watchlist->media as $media)
+            <div class="col-span-1 mr-4 mb-6 transform transition duration-500 hover:scale-105">
                 <a href="/media/{{ $media->id }}"><img class="relative rounded" width="85%" height="85%" src="{{ $media->omdb->poster }}" /></a>
             </div>
+            @endforeach
+        </div>
+        @endif
+        @endforeach
+        @endforeach
 
+
+        <hr class="mb-6" />
+
+        <a href="/profile/{{ auth()->user()->username }}" class="mb-6 text-xl flex hover:underline">Watched</a>
+        <div class="lg:grid lg:grid-cols-5">
+            @foreach ($user->media as $media)
+            <div class="col-span-1 mr-4 mb-6 transform transition duration-500 hover:scale-105">
+                <a href="/media/{{ $media->id }}"><img class="relative rounded" width="85%" height="85%" src="{{ $media->omdb->poster }}" /></a>
+            </div>
             @endforeach
         </div>
     </x-sidebar.home-sidebar>
