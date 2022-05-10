61bee853eeac3caef74ae169d0945f8c3937d4c0
Tom Hart <tommy-hart@hotmail.com>
2022-05-10 20:48:04 +0100

    add watched posters component

---
+++ resources/views/components/watched.blade.php
@@ -0,0 +1,8 @@
+<a href="/profile/{{ $user->username }}" class="mb-6 text-xl flex hover:underline">Watched</a>
+<div class="lg:grid lg:grid-cols-5">
+    @foreach ($user->media as $media)
+    <div class="col-span-1 mr-4 mb-6 transform transition duration-500 hover:scale-105">
+        <a href="/media/{{ $media->id }}"><img class="relative rounded" width="85%" height="85%" src="{{ $media->omdb->poster }}" /></a>
+    </div>
+    @endforeach
+</div>
