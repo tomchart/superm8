1087eea61220e5bb915ae37075404697140ddd7b
Tom Hart <tommy-hart@hotmail.com>
2022-05-11 16:49:23 +0100

    make OmdbInfo searchable

--- app/Models/OmdbInfo.php
+++ app/Models/OmdbInfo.php
@@ -4,10 +4,11 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class OmdbInfo extends Model
 {
-    use HasFactory;
+    use HasFactory, Searchable;
 
     protected $table = 'omdb_info';
 
