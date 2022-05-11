02b65272c5fffedd405dabdc1cef04630c2fc316
Tom Hart <tommy-hart@hotmail.com>
2022-05-11 13:09:19 +0100

    refactor FetchMissingOmdbInfo functionality into OMDb service

--- app/Services/OMDb.php
+++ app/Services/OMDb.php
@@ -3,17 +3,42 @@
 namespace App\Services;
 
 use Illuminate\Support\Facades\Http;
+use App\Models\OmdbInfo;
 
 
 class OMDb
 {
     public function searchTitle(String $title)
     {
-        // search for $title and return result
+        // search for $title 
         $response = Http::get(config('services.omdb.base_uri'), [
             'apikey' => config('services.omdb.key'),
             't' => $title,
         ]);
-        return $response->object();
+
+        // create OmdbInfo from $response
+        $OmdbInfo = new OmdbInfo($this->parse($response->object()));
+        return $OmdbInfo;
+    }
+    protected function parse($response)
+    {
+        $omdbArray = [];
+
+        // store rotten tomatoes rating if exists
+        if ($response->Ratings) {
+            foreach ($response->Ratings as $rating) {
+                if ($rating->Source == 'Rotten Tomatoes') {
+                    $omdbArray["rottenTomatoesRating"] = $rating->Value;
+                };
+            };
+        }
+
+        // loop over response, dropping Ratings, and store in array
+        foreach ($response as $key => $value) {
+            if ($key != "Ratings") {
+                $omdbArray[$key] = $value;
+            };
+        }
+        return $omdbArray;
     }
 }
