b6ae01abb61df1589fb643d178ce670283dd9593
Tom Hart <tommy-hart@hotmail.com>
2022-05-11 16:54:13 +0100

    add js search.vue, scout config

---
+++ resources/js/components/search.vue
@@ -0,0 +1,21 @@
+<template>
+    <ais-instant-search :search-client="searchClient" index-name="omdb_info">
+        <ais-search-box placeholder="Search contacts..."></ais-search-box>
+    </ais-instant-search>
+</template>
+
+<script>
+import algoliasearch from "algoliasearch/lite";
+import InstantSearch from "vue-instantsearch/vue3/es";
+
+export default {
+    data() {
+        return {
+            searchClient: algoliasearch(
+                process.env.MIX_ALGOLIA_APP_ID,
+                process.env.MIX_ALGOLIA_SEARCH
+            ),
+        };
+    },
+};
+</script>
