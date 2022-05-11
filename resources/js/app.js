1a8383bde8ea6228a6408aab3cd4137663b6c742
Tom Hart <tommy-hart@hotmail.com>
2022-05-11 16:53:39 +0100

    add js/app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,10 @@
-require('./bootstrap');
+import { createApp } from "vue";
+import InstantSearch from "vue-instantsearch/vue3/es";
+import search from "./components/search.vue";
+
+const app = createApp({}).use(InstantSearch);
+app.component("VueInstantSearch", InstantSearch);
+app.component("search", require("./components/search.vue").default);
+const mountedApp = app.mount("#app");
+
+require("./bootstrap");
