d7d78b84d33188e049e7b703ae56fb3018d55295
Tom Hart <tommy-hart@hotmail.com>
2022-05-17 12:06:01 +0100

    amend store & destroy methods in MediaUserController

--- app/Http/Controllers/MediaUserController.php
+++ app/Http/Controllers/MediaUserController.php
@@ -13,7 +13,8 @@ class MediaUserController extends Controller
 
     public function store(User $user)
     {
-        $user->media()->attach(request()->media);
+        $match = $this->findIfExists(request()->name);
+        $user->media()->attach($match);
         return back()->with('success', 'media added');
     }
 
@@ -21,5 +22,16 @@ class MediaUserController extends Controller
     {
         $user->media()->detach($media);
         return back()->with('success', 'media removed');
+    }
+
+    protected function findIfExists(String $title)
+    {
+        $match = Media::search($title)->get()->first();
+        // will this actually be falsey if unmatched?
+        if ($match) {
+            return $match;
+        } else {
+            return false;
+        }
     }
 }
