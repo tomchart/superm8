15d2d262472316894b089f49396d5ed464ae041f
Tom Hart <tommy-hart@hotmail.com>
2022-05-17 12:09:33 +0100

    update table components (darkmode)

--- resources/views/components/table/media.blade.php
+++ resources/views/components/table/media.blade.php
@@ -1,8 +1,8 @@
 <td class="px-6 py-4 whitespace-nowrap">
     <div class="flex items-center">
-        <div class="text-sm font-medium text-gray-900 hover:underline">
+        <div class="text-sm font-medium text-white hover:underline">
             <a class="flex flex-row" href="/media/{{ $media->id }}">
-                {{ $media->name }}
+                {{ $media->Title }}
                 <x-rating.rating :media="$media" :ratings="$ratings" class="flex flex-row px-2 py-1.5" />
             </a>
         </div>
--- resources/views/components/table/table.blade.php
+++ resources/views/components/table/table.blade.php
@@ -3,9 +3,9 @@
     <div class="col-span-8 mt-6">
         <div class="-my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
             <div class="py-2 align-middle inline-block min-w-full sm:px-6 lg:px-8">
-                <div class="shadow overflow-hidden border-b border-gray-200 sm:rounded-lg">
+                <div class="shadow overflow-hidden border-b border-gray-600 sm:rounded-lg">
                     <table class="min-w-full divide-y divide-gray-200">
-                        <tbody class="bg-white divide-y divide-gray-200">
+                        <tbody class="bg-gray-800 divide-y divide-gray-600">
                             {{ $slot }}
                         </tbody>
                     </table>
