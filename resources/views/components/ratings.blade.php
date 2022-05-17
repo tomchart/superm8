552e03819bbf9222edf4daf4f61ec190468cb305
Tom Hart <tommy-hart@hotmail.com>
2022-05-17 12:10:15 +0100

    update ratings component (darkmode)

--- resources/views/components/ratings.blade.php
+++ resources/views/components/ratings.blade.php
@@ -1,24 +1,24 @@
-@if ($omdb->Awards)
-<p class="text-sm mb-6">{{ $omdb->Awards }}</p>
+@if ($media->Awards)
+<p class="text-sm mb-6">{{ $media->Awards }}</p>
 @endif
 
-@if ($omdb->imdbRating)
-<a class="flex inline" href="https://www.imdb.com/title/{{ $omdb->imdbID }}">
+@if ($media->imdbRating)
+<a class="flex inline" href="https://www.imdb.com/title/{{ $media->imdbID }}">
     <x-icons name="imdb" />
-    <p class="text-sm mb-2 ml-2">{{ $omdb->imdbRating . ' (' . $omdb->imdbVotes . ' votes)' }}</p>
+    <p class="text-sm mb-2 ml-2">{{ $media->imdbRating . ' (' . $media->imdbVotes . ' votes)' }}</p>
 </a>
 @endif
 
-@if ($omdb->rottenTomatoesRating)
+@if ($media->rottenTomatoesRating)
 <div class="flex inline">
     <x-icons name="tomato" href="make this link to film on rt?" />
-    <p class="text-sm mb-2 ml-1">{{ $omdb->rottenTomatoesRating }}</p>
+    <p class="text-sm mb-2 ml-1">{{ $media->rottenTomatoesRating }}</p>
 </div>
 @endif
 
-@if ($omdb->Metascore != "N/A")
+@if ($media->Metascore != "N/A")
 <div class="flex inline">
     <x-icons name="metacritic" href="make this link to film on mc?" />
-    <p class="text-sm mb-2 ml-1">{{ $omdb->Metascore }}</p>
+    <p class="text-sm mb-2 ml-1">{{ $media->Metascore }}</p>
 </div>
 @endif
