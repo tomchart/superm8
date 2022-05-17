51b4c8e7fab76c9e643c216746d9150162472f2e
Tom Hart <tommy-hart@hotmail.com>
2022-05-17 12:03:32 +0100

    remove FetchMissingOmdbInfo middleware, replace with FetchMissingMediaInfo, implement MediaApi abstraction to fetch

---
+++ app/Http/Middleware/FetchMissingMediaInfo.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use App\Services\MediaApi;
+use App\Models\Rating;
+
+class FetchMissingMediaInfo
+{
+    /**
+     * Create a new class instance.
+     *
+     * @param  \App\Contracts\MediaApi  $mediaAPI
+     * @return void
+     */
+    public function __construct(MediaApi $mediaAPI)
+    {
+        $this->mediaAPI = $mediaAPI;
+    }
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure(\Illuminate\Http\Request): (\Illuminate\Http\Response|\Illuminate\Http\RedirectResponse)  $next
+     * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
+     */
+    // not quite working - need to instantiate MediaApi rather than expect it as a var
+    public function handle(Request $request, Closure $next)
+    {
+        if (!$request->media) {
+            // validate search data
+            $attributes = $this->validateSearch();
+
+            $mediaInfo = $this->mediaAPI->searchTitle($attributes['name']);
+
+            // return rating from table
+            $mediaInfo->rating()->associate(Rating::where('id', '=', $attributes['rating_ebert'])->first());
+            $mediaInfo->save();
+            // merge newly created mediaInfo model to request array
+            $request->merge(['mediaInfo' => $mediaInfo]);
+            return $next($request);
+        } else {
+            // exists, do nothing?
+            return $next($request);
+        };
+        return $next($request);
+    }
+    protected function validateSearch()
+    {
+        return request()->validate([
+            'name' => ['required', 'max:255'],
+            'type_id' => [],
+            'rating_ebert' => [],
+        ]);
+    }
+}
