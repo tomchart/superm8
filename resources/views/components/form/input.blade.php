d8f168de60942054c22d553f50656f7973eed83e
Tom Hart <tommy-hart@hotmail.com>
2022-05-17 12:08:35 +0100

    update form components (darkmode)

--- resources/views/components/form/input.blade.php
+++ resources/views/components/form/input.blade.php
@@ -1,5 +1,5 @@
 @props (['name'])
 <x-form.field>
-    <input class="border border-gray-200 p-2 w-full rounded mb-6" name="{{ $name }}" id="{{ $name }}" {{ $attributes(['value' => old($name)]) }}>
+    <input class="border border-gray-600 bg-gray-800 p-2 w-full rounded mb-6" name="{{ $name }}" id="{{ $name }}" {{ $attributes(['value' => old($name)]) }}>
     <x-form.error name="{{ $name }}" />
 </x-form.field>
