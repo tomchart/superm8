94d4133aea919099d80211c400af83fe28a13983
Tom Hart <tommy-hart@hotmail.com>
2022-05-19 17:17:09 +0100

    use daisyUI, set dracula theme, amend navbar in layout component

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -1,5 +1,7 @@
 <!doctype html>
 
+<html data-theme="dracula">
+
 <title>superm8</title>
 <meta charset="UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -17,25 +19,51 @@
     }
 </style>
 
-<body style="font-family: Open Sans, sans-serif" class="bg-gray-800 text-white">
-    <section class="px-6 py-6">
-        <nav class="md:flex md:justify-between md:items-center">
-            <div>
-                <a href="/" class="font-semibold italic text-2xl hover:underline">superm8</a>
-            </div>
-            <div class="mt-8 md:mt-0 mr-6 flex items-center space-x-6">
+<body style="font-family: Open Sans, sans-serif" class="bg-base-100 text-white">
+    <section class="">
+        <nav class="text-black">
+            <div class="navbar bg-secondary">
+                <div class="flex-1">
+                    <a href="/" class="btn btn-ghost normal-case text-xl italic ml-4">superm8</a>
+                </div>
                 @auth
-                <a href="/account" class="hover:underline">My Account</a>
-                <a href="/logout" class="hover:underline">Log out</a>
-                @else
-                <a href="/login" class="hover:underline">Login</a>
-                <a href="/register" class="hover:underline">Register</a>
+                <div class="flex-none">
+                    <ul class="menu menu-horizontal p-0 mr-4">
+                        <li tabindex="0">
+                            <a class="mr-4">
+                                {{ auth()->user()->username }}
+                                <svg class="fill-current" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24">
+                                    <path d="M7.41,8.58L12,13.17L16.59,8.58L18,10L12,16L6,10L7.41,8.58Z" />
+                                </svg>
+                            </a>
+                            <ul class="p-2 bg-secondary-focus">
+                                <li><a href="/account">account information</a></li>
+                                <li><a href="/clubs">club management</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="/logout">logout</a></li>
+                    </ul>
+                </div>
                 @endauth
             </div>
-
+            <!-- <div class="navbar bg-primary text-primary-content"> -->
+            <!--     <a href="/" class="btn btn-ghost normal-case text-xl italic" hover:underline>superm8</a> -->
+            <!-- </div> -->
+            <!-- <div class="mt-8 md:mt-0 mr-6 flex items-center space-x-6"> -->
+            <!--     @auth -->
+            <!--     <a href="/account" class="hover:underline">My Account</a> -->
+            <!--     <a href="/logout" class="hover:underline">Log out</a> -->
+            <!--     @else -->
+            <!--     <a href="/login" class="hover:underline">Login</a> -->
+            <!--     <a href="/register" class="hover:underline">Register</a> -->
+            <!--     @endauth -->
+            <!-- </div> -->
+            <!---->
         </nav>
 
         {{ $slot }}
 
     </section>
 </body>
+
+</html>
