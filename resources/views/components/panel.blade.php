9918009ccab987b98fde71b922f0b5ff660680ec
Tom Hart <tommy-hart@hotmail.com>
2022-05-19 17:17:21 +0100

    amend bg colour in panel component

--- resources/views/components/panel.blade.php
+++ resources/views/components/panel.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes(['class' => 'border border-gray-600 p-6 rounded-xl bg-gray-900']) }}>
+<div {{ $attributes(['class' => 'border border-gray-600 p-6 rounded-xl bg-base-300']) }}>
     {{ $slot }}
 </div>
