0aeb7dfe899bc057c0b6e99e3050ef073c625b73
Tom Hart <tommy-hart@hotmail.com>
2022-05-19 16:50:14 +0100

    update components used in club views

--- resources/views/club/show.blade.php
+++ resources/views/club/show.blade.php
@@ -10,12 +10,13 @@
 
         <div class="inline-flex">
             <!-- make this only for club owner with a Gate -->
-            <a href="/admin/club/{{ $club->id }}/edit" class="mt-8 py-2 px-4 mr-8 hover:underline">edit club</a>
+            <a href="/admin/club/{{ $club->id }}/edit" class="btn btn-outline btn-secondary mt-10 mr-4">edit club</a>
+
 
             <form method="POST" action="/invite/{{ $club->slug }}" class="">
                 @csrf
                 <div class="inline-flex">
-                    <x-form.button>create invite</x-form.button>
+                    <x-form.button class="btn-secondary mt-4">create invite</x-form.button>
                     @if (session()->has('invite'))
                     <p class="mt-14 ml-6">Invite code: {{ session('invite')->code }}</p>
                     @endif
