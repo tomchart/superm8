c96adb786dfba25aad115469e02b7279312a5113
Tom Hart <tommy-hart@hotmail.com>
2022-05-19 17:15:15 +0100

    use daisyUI components in profile.add-watched component

--- resources/views/components/profile/add-watched.blade.php
+++ resources/views/components/profile/add-watched.blade.php
@@ -2,28 +2,29 @@
     @csrf
     @method('POST')
 
-    <div class="lg:grid lg:grid-cols-12">
-        <div class="col-span-7">
-            <x-form.label name="add something" />
-            <x-form.input name="name" required />
+    <div class="lg:grid lg:grid-cols-12 mb-4 text-black">
+        <div class="col-span-6">
+            <x-form.input name="name" placeholder="enter title..." class="mt-2" required />
         </div>
 
-        <div class="inline-flex col-span-5">
-            <select name="type_id" id="type_id" class="mt-4 ml-4 mr-4 bg-gray-900">
+
+        <div class="inline-flex col-span-6">
+            <select name="type_id" id="type_id" class="select bg-secondary mt-8 mr-2 ml-4">
+                <option disabled selected>media type</option>
                 @foreach ($mediaTypes as $type)
                 <option value="{{ $type->id }}">{{ ucwords($type->type) }}</option>
                 @endforeach
             </select>
 
-            <select name="rating_ebert" id="rating_ebert" class="mt-4 ml-4 mr-4 bg-gray-900">
-                <option value="" disabled selected hidden>rating</option>
+            <select name="rating_ebert" id="rating_ebert" class="select bg-secondary mt-8 ml-2 mr-2">
+                <option disabled selected>ebert rating</option>
                 <option value="">unrated</option>
                 @foreach ($ratings as $rating)
                 <option value="{{ $rating->id }}">{{ $rating->rating }}</option>
                 @endforeach
             </select>
 
-            <x-form.button>add</x-form.button>
+            <x-form.button class="mt-2">add to watched</x-form.button>
         </div>
 
     </div>
