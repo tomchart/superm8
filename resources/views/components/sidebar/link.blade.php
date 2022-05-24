d3507cebc73ce2265c68a1e2ec95ea453d945b35
Tom Hart <tommy-hart@hotmail.com>
2022-05-24 14:00:13 +0100

    remove old sidebar components

--- resources/views/components/sidebar/link.blade.php
+++
@@ -1,4 +0,0 @@
-@props (['url', 'str'])
-<div class="mt-2">
-    <x-sidebar.item href="/{{ $url }}" :active='request()->is("{$url}")'>{{ $str }}</x-sidebar.item>
-</div>
--- resources/views/components/sidebar/main.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="lg:grid lg:grid-cols-12 px-6 py-8">
-    <x-panel class="col-span-3 lg:pt-14 mb-10 mr-4">
-        {{ $side }}
-    </x-panel>
-
-    <x-panel class="col-span-9 lg:pt-14 mb-10">
-        {{ $slot }}
-    </x-panel>
-</div>
