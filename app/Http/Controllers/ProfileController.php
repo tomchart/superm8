715d9963b537be9933e00a7bb172d45dd569ebd5
Tom Hart <tommy-hart@hotmail.com>
2022-05-25 13:22:49 +0100

    add handling for avatar upload in ProfileController@update

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -20,17 +20,25 @@ class ProfileController extends Controller
 
     public function update()
     {
+        $path = null;
+        if (request()->file('avatar')) {
+            $path = request()->file('avatar')->store('avatars');
+        }
         $user = auth()->user();
         $attributes = request()->validate([
             'name' => ['min:3', 'max:255'],
             'username' => ['min:3', 'max:50', Rule::unique('users', 'username')->ignore($user->id)],
             'description' => ['min:0', 'max:255'],
+            'avatar' => ['image'],
         ]);
         // need to throw something here if user inputs bad data
         // atm just return regardless
         foreach ($attributes as $key => $value) {
             $user->$key = $value;
         };
+        if ($path) {
+            $user->avatar = $path;
+        }
         $user->save();
         return redirect('/profile/' . $attributes['username'])->with('success', 'Account edited.');
     }
