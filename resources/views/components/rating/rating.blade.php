11a809a065c946e3ecb7635827a16eb7186e5618
Tom Hart <tommy-hart@hotmail.com>
2022-05-26 14:06:33 +0100

    rewrite rating.rating component (still hacky)

--- resources/views/components/rating/rating.blade.php
+++ resources/views/components/rating/rating.blade.php
@@ -1,16 +1,11 @@
-<p>
+<!-- this is very hacky and could be done about a million times better -->
 <div class="{{ $class }}">
-
     @if ($media->rating)
-    @while ($media->rating->rating > 0)
-    @if ($media->rating->rating > 0.5)
-    <x-icons name="star-full" />
-    @else
-    <x-icons name="star-half" />
+    @for ($i = 0; $i < floor($media->rating->getOriginal('rating')); $i++)
+        <x-icons name="star-full" />
+    @endfor
+    @if ($i < $media->rating->getOriginal('rating'))
+        <x-icons name="star-half" />
     @endif
-    @php $media->rating->rating--; @endphp
-    @endwhile
     @endif
-
 </div>
-</p>
