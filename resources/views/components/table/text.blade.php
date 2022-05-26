d9627f01563a5b18c097e8bf9d9ef42307e81faa
Tom Hart <tommy-hart@hotmail.com>
2022-05-26 14:06:11 +0100

    remove old table.media&row&text components

--- resources/views/components/table/text.blade.php
+++
@@ -1,9 +0,0 @@
-<td class="px-6 py-4 whitespace-nowrap">
-    <div class="flex items-center">
-        <div class="text-sm font-medium text-white hover:underline">
-            <a href="{{ $href }}">
-                {{ $text }}
-            </a>
-        </div>
-    </div>
-</td>
