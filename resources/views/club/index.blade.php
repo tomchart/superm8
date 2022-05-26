e2afd559d88e6e977683a14b2f912ab9ca6e7a4b
Tom Hart <tommy-hart@hotmail.com>
2022-05-26 14:07:09 +0100

    use daisyui table components in club.index view

--- resources/views/club/index.blade.php
+++ resources/views/club/index.blade.php
@@ -1,12 +1,28 @@
 <x-layout>
-    <x-table.table label="clubs">
-        @foreach ($clubs as $club)
-        <x-table.row>
-            <x-table.text href="/club/{{ $club->slug }}" text="{{ $club->name }}" />
-            <x-table.text href="/admin/club/{{ $club->id }}/edit" text="edit" />
-            <x-table.button action="/admin/club/{{ $club->id }}/{{ auth()->user()->id }}" method="DELETE" text="leave" />
-            <x-table.button action="/admin/club/{{ $club->id }}" method="DELETE" text="delete" />
-        </x-table.row>
-        @endforeach
+    <x-table.table>
+        <x-slot name="head">
+            <th></th>
+            <th>Club Name</th>
+            <th></th>
+            <th></th>
+            <th></th>
+        </x-slot>
+        <x-slot name="row">
+            @foreach ($clubs as $club)
+            <tr>
+                <th></th>
+                <td><a class="hover:underline" href="/club/{{ $club->slug }}">{{ $club->name }}</a></td>
+                <th>
+                    <x-link-button class="btn-xs btn-secondary" href="/admin/club/{{ $club->id }}/edit" text="edit" />
+                </th>
+                <th>
+                    <x-table.button action="/admin/club/{{ $club->id }}/{{ auth()->user()->id }}" method="DELETE" btnClass="btn btn-xs btn-primary" btnText="leave" />
+                </th>
+                <th>
+                    <x-table.button action="/admin/club/{{ $club->id }}" method="DELETE" btnClass="btn btn-xs btn-error" btnText="delete" />
+                </th>
+            @endforeach
+            </tr>
+        </x-slot>
     </x-table.table>
 </x-layout>
