8452271accb920723baa24155f78935ccfe39834
Tom Hart <tommy-hart@hotmail.com>
2022-06-10 15:32:00 +0100

    add cannes-resize

--- resources/views/components/splash.blade.php
+++ resources/views/components/splash.blade.php
@@ -1,7 +1,7 @@
 <div class="ml-56 mr-56 mt-20 absolute inset-0 z-10">
     <div class="hero bg-base-200 rounded-xl border border-gray-600 flex">
         <div class="hero-content flex-col lg:flex-row">
-            <img src="{{ asset('storage/images/cannes-resize.jpeg') }}" class="max-w-sm rounded-lg shadow-2xl scale-50" />
+            <img src="{{ url('/images/cannes-resize.jpeg') }}" class="max-w-sm rounded-lg shadow-2xl scale-50" />
             <div>
                 <h1 class="text-5xl font-bold">Uh oh!</h1>
                 <p class="py-6">You currently have no clubs or watched media 💔</p>
