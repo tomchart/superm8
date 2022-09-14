9b7b2175d418d917b65b432341d68d6e2d58f173
Tom Hart <tommy-hart@hotmail.com>
2022-09-14 12:51:27 +0100

    add title to watch/unwatch flashes

--- app/Http/Controllers/MediaWatchlistController.php
+++ app/Http/Controllers/MediaWatchlistController.php
@@ -13,9 +13,9 @@ class MediaWatchlistController extends Controller
         $watchlist->media()->updateExistingPivot($media->id, ['watched' => request()->inverse_status]);
         AddWatchedFromWatchlist::dispatch(request()->inverse_status, $watchlist->club, $media);
         if (request()->inverse_status) {
-            return redirect(url()->previous() . '#list')->with('success', 'media watched');
+            return redirect(url()->previous() . '#list')->with('success', $media->Title . ' watched');
         } else {
-            return redirect(url()->previous() . '#list')->with('alert', 'media unwatched');
+            return redirect(url()->previous() . '#list')->with('alert', $media->Title . ' unwatched');
         }
     }
 
